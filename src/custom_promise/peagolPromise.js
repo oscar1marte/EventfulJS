e29bf82010d6bfa54e4344d0d41fb3b70b21f668
oscar1marte <oscar1marte@gmail.com>
2025-04-14 09:04:42 -0400

    feat: add promise functionality (17/25 tests passed)

--- src/custom_promise/peagolPromise.js
+++ src/custom_promise/peagolPromise.js
@@ -1,42 +1,71 @@
-const isPromise = (value) => {
-  if (value && typeof value.then == 'function') {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 class PeagolPromise {
   constructor(executor) {
     this.state = 'PENDING';
     this.pendings = [];
+    this.errs = [];
     this.value;
-    executor(this.resolve.bind(this), () => { });
-    //setTimeout(() => {
-    //this.resolve();
-    //}, 1000);
+    this.reason;
+
+    try {
+      executor(this.resolve.bind(this), this.reject.bind(this));
+    } catch (e) {
+      this.reject(e);
+    }
   }
 
   then(onFulfilled, onRejected) {
-    if (typeof onFulfilled != 'function') {
-      return this
+    let nextPromise = new PeagolPromise(() => { });
+    if (typeof onFulfilled === 'function' && !this.reason) {
+      if (this.state === 'PENDING') {
+        this.pendings.push(onFulfilled);
+      } else {
+        try {
+          value = onFulfilled(this.value);
+          nextPromise.state = 'FULFILLED';
+          nextPromise.value = value;
+        } catch (e) {
+          nextPromise.state = 'REJECTED';
+        }
+        return nextPromise;
+      }
     }
-    if (this.state === 'PENDING') {
-      this.pendings.push(onFulfilled);
-    } else {
-      this.value = onFulfilled(this.value);
+    if (typeof onRejected === 'function' && !this.value) {
+      if (this.state === 'PENDING') {
+        this.errs.push(onRejected);
+      } else {
+        try {
+          value = onRejected(this.reason);
+          nextPromise.state = 'FULFILLED';
+          nextPromise.value = value;
+        } catch (e) {
+          nextPromise.state = 'REJECTED';
+          nextPromise.reason = e;
+        }
+        return nextPromise;
+      }
     }
-    return this
+    return nextPromise;
   }
 
   resolve(value) {
     if (this.state === 'PENDING') {
       this.value = value;
+      this.state = 'FULFILLED';
       for (let i = 0, ii = this.pendings.length; i < ii; i++) {
         let callback = this.pendings[i];
         callback(this.value);
       }
-      this.state = 'FULFILLED';
+    }
+  }
+
+  reject(reason) {
+    if (this.state === 'PENDING') {
+      this.reason = reason;
+      this.state = 'REJECTED';
+      for (let j = 0, jj = this.errs.length; j < jj; j++) {
+        let errback = this.errs[j];
+        errback(this.reason);
+      }
     }
   }
 }
